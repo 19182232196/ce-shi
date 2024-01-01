834ab3162c5b95050b11b90445ab740e3f51fc87
Nenhung <83660804+19182232196@users.noreply.github.com>
2024-01-01 16:15:24 +0800

    Add files via upload

--- mybatis_parameter/src/main/java/com/mapper/UserMapper.java
+++ mybatis_parameter/src/main/java/com/mapper/UserMapper.java
@@ -1,11 +1,23 @@
 package com.mapper;
 
 import com.pojo.User;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserMapper {
-    User getUserByusername(String username);
+    User getUserByusername(@Param("username") String username);
     User checkLogin(String username, String password);
     User checkLoginByMap(Map<String, Object> map);
+    void insertUser(User user);
+    User checkLoginByParam(@Param("username") String username,@Param("password") String password);
+    User getUserById(@Param("id") Integer id);
+    List<User> getAllUser();
+    Integer getUserCount();
+    Map<String, Object> getUserByIdToMap(@Param("id") Integer id);
+//    List<Map<String, Object>> getAllUserMap();
+    @MapKey("id")
+    Map<String, Object> getAllUserMap();
 }
