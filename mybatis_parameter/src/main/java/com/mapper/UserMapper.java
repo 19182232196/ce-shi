077057b490cac7e40bdbe9ed5f20a17a6e5cb687
Nenhung <83660804+19182232196@users.noreply.github.com>
2024-01-09 20:11:17 +0800

    Add files via upload

--- mybatis_parameter/src/main/java/com/mapper/UserMapper.java
+++ mybatis_parameter/src/main/java/com/mapper/UserMapper.java
@@ -9,15 +9,28 @@ import java.util.Map;
 
 public interface UserMapper {
     User getUserByusername(@Param("username") String username);
+
     User checkLogin(String username, String password);
+
     User checkLoginByMap(Map<String, Object> map);
+
     void insertUser(User user);
-    User checkLoginByParam(@Param("username") String username,@Param("password") String password);
+
+    User checkLoginByParam(@Param("username") String username, @Param("password") String password);
+
     User getUserById(@Param("id") Integer id);
+
     List<User> getAllUser();
+
     Integer getUserCount();
+
     Map<String, Object> getUserByIdToMap(@Param("id") Integer id);
-//    List<Map<String, Object>> getAllUserMap();
+
+    //    List<Map<String, Object>> getAllUserMap();
     @MapKey("id")
     Map<String, Object> getAllUserMap();
+
+    List<User> getUserByLike(@Param("mohu") String mohu);
+    void deleteMoveUser(@Param("ids") String ids);
+    List<User> getUserList(@Param("tablename") String tablename);
 }
